6a08e0e432942f2345f1b2fecb6c57303b491e96
Empa <emanuel@empa.xyz>
2021-12-11 19:03:56 +0100

    Modify deploy script to work with lsyncd instead of manually moving to srv.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 
 cd "$(dirname "$0")" || exit 1
 
+#.venv/bin/python freeze.py &&
+#rsync -azvh --delete build/ empa@empa.xyz:~/site &&
+#ssh empa@empa.xyz -t "sudo sh -c 'chown -R nginx:nginx site && rm -rf /srv/www/empa.xyz && mv site /srv/www/empa.xyz'"
+#rm -rf build
+
 .venv/bin/python freeze.py &&
-rsync -azvh --delete build/ empa@empa.xyz:~/site &&
-ssh empa@empa.xyz -t "sudo sh -c 'chown -R nginx:nginx site && rm -rf /srv/www/empa.xyz && mv site /srv/www/empa.xyz'"
+    rsync -azvh --delete build/ empa@empa.xyz:~/.site
 rm -rf build
