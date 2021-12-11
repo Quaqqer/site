2aa69505f3b9530de773461bcdbd66171d025e98
Empa <emanuel@empa.xyz>
2021-12-11 14:56:00 +0100

    Add freezer and update deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cd $(dirname $0)
+cd "$(dirname "$0")" || exit 1
 
-rsync -azvh --delete --chown=nginx:nginx site/ empa@empa.xyz:~/site && \
+./freeze.py &&
+rsync -azvh --delete build/ empa@empa.xyz:~/site &&
 ssh empa@empa.xyz -t "sudo sh -c 'chown -R nginx:nginx site && rm -rf /srv/www/empa.xyz && mv site /srv/www/empa.xyz'"
